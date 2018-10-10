9d5b2920c3194784916901544cd7fe365dcff960
Stavila Alexandru <alexandru.stavila@fortech.ro>
2018-10-10 15:54:08 +0300

    New API: add_event

--- event/api.py
+++ event/api.py
@@ -19,3 +19,14 @@ class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     # permission_classes = (permissions.IsAuthenticated,)
+
+
+@api_view(['POST'])
+def add_event(request):
+    if request.method == 'POST':
+        payment_list = request.data['payment_list']
+        event_serializer = EventSerializer(date=request.data)
+        if event_serializer.is_valid():
+            event = event_serializer.save()
+            return Response(event_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(event_serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
