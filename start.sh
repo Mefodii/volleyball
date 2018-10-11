daee1269125343830d4af44fa44072df076fa9e2
Stavila Alexandru <alexandru.stavila@fortech.ro>
2018-10-11 10:44:59 +0300

    Working script for server

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source /home/mefodii/python_projects/volleyball/volleyball-venv/bin/activate
-cd /home/mefodii/python_projects/volleyball/volleyball
-python3 manage.py runserver 173.212.252.36:8000
+source ../volleyball-venv/bin/activate
+python manage.py runserver 173.212.252.36:8000
